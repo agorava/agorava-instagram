5729d1178dee544b4f8bcd6c7ec7d75af97e127e
wkeil <werner.keil@gmx.net>
2014-05-17 22:37:32 +0200

    1: Update to Agorava 0.7.1
    Task-Url: https://github.com/keilw/agorava-instagram/issues/issue/1

--- agorava-instagram-api/src/main/java/org/agorava/instagram/InstagramApi.java
+++ agorava-instagram-api/src/main/java/org/agorava/instagram/InstagramApi.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Agorava
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.agorava.instagram;
 
 import org.agorava.api.oauth.application.OAuthAppSettings;
@@ -6,9 +22,9 @@ import org.agorava.api.service.Preconditions;
 import org.agorava.spi.ProviderConfigOauth20Final;
 
 /**
- * User: Dje
- * Date: 20/04/13
- * Time: 11:35
+ * @author Werner Keil
+ * Date: 17/05/14
+ * Time: 22:35
  */
 @Instagram
 public class InstagramApi extends ProviderConfigOauth20Final {
--- agorava-instagram-api/src/main/java/org/agorava/instagram/InstagramUserService.java
+++ agorava-instagram-api/src/main/java/org/agorava/instagram/InstagramUserService.java
@@ -1,15 +1,29 @@
+/*
+ * Copyright 2014 Agorava
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.agorava.instagram;
 
 import org.agorava.instagram.model.InstagramProfile;
 import org.agorava.instagram.model.InstagramProfileList;
 import org.agorava.spi.UserProfileService;
 
-import java.util.List;
-
 /**
- * User: Dje
- * Date: 19/04/13
- * Time: 23:46
+ * @author Werner Keil
+ * Date: 17/05/14
+ * Time: 22:26
  */
 public interface InstagramUserService extends UserProfileService {
 
